f707869194f6ecf73ceaf420f4e7c641fcd8ffbb
mathewgries <mathewgries@gmail.com>
2019-03-03 16:32:53 -0500

    Set up basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import { Nav, Navbar, NavItem } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import Routes from '../Routes'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          App
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='App container'>
+				<Navbar fluid collapseOnSelect>
+					<Navbar.Header>
+						<Navbar.Brand>
+							<Link to='/'>Work Orders</Link>
+						</Navbar.Brand>
+						<Navbar.Toggle/>
+					</Navbar.Header>
+					<Navbar.Collapse>
+						<Nav pullRight>
+							<LinkContainer to='/signup'>
+								<NavItem >Sign Up</NavItem>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<NavItem>Login</NavItem>
+							</LinkContainer>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+				<Routes />
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Home.css'
+
+export default class Home extends Component {
+    render(){
+        return(
+            <div className='Home'>
+                <div className='lander'>
+                <h1>Work Orders</h1>
+                <p>Keep track of your projects and clients</p>
+                </div>
+            </div>
+        )
+    }
+}
