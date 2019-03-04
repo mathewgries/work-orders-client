4e2619a80e127daadd9c7b644900d7eccd3adec2
mathewgries <mathewgries@gmail.com>
2019-03-03 20:40:34 -0500

    Fixed types in home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,15 +1,88 @@
-import React, { Component } from 'react'
-import './Home.css'
+import React, { Component } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
+import { API } from 'aws-amplify'
+import "./Home.css";
 
 export default class Home extends Component {
-    render(){
-        return(
-            <div className='Home'>
-                <div className='lander'>
-                <h1>Work Orders</h1>
-                <p>Keep track of your projects and clients, and maybe your money too</p>
-                </div>
-            </div>
-        )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      workorders: []
+    };
+  }
+
+  async componentDidMount() {
+    if (!this.props.isAuthenticated) {
+      return;
     }
+  
+    try {
+      const workorders = await this.workorders();
+      this.setState({ workorders });
+    } catch (e) {
+      alert(e);
+    }
+  
+    this.setState({ isLoading: false });
+  }
+  
+  workorders() {
+    return API.get('workorders', '/workorders');
+  }
+
+  renderWorkordersList(workorders) {
+    return [{}].concat(workorders).map(
+      (workorder, i) =>
+        i !== 0
+          ? <LinkContainer
+              key={workorder.workorderId}
+              to={`/workorders/${workorder.workorderId}`}
+            >
+              <ListGroupItem header={workorder.content.trim().split("\n")[0]}>
+                {"Created: " + new Date(workorder.createdAt).toLocaleString()}
+              </ListGroupItem>
+            </LinkContainer>
+          : <LinkContainer
+              key="new"
+              to="/workorders/new"
+            >
+              <ListGroupItem>
+                <h4>
+                  <b>{"\uFF0B"}</b> Create a new workorder
+                </h4>
+              </ListGroupItem>
+            </LinkContainer>
+    );
+  }
+
+  renderLander() {
+    return (
+      <div className="lander">
+        <h1>Scratch</h1>
+        <p>Manage your projects, clients, and account</p>
+      </div>
+    );
+  }
+
+  renderWorkorders() {
+    return (
+      <div className="workorders">
+        <PageHeader>Your Workorders</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderWorkordersList(this.state.workorders)}
+        </ListGroup>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        {this.props.isAuthenticated ? this.renderWorkorders() : this.renderLander()}
+      </div>
+    );
+  }
 }
